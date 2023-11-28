cf69d6fa084f3607100af08677bc6c6a29e999a7
dangvietdung6 <dangviet6112@gmail.com>
2023-11-28 17:54:11 +0700

    prj1

---
+++ HCbooking/Booking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HCbooking
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime BookingTime { get; set; }
+    }
+
+}
---
+++ HCbooking/BookingController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace HCbooking
+{
+    public class BookingController : Controller
+    {
+        private static List<Booking> bookings = new List<Booking>();
+
+        public IActionResult Index()
+        {
+            return View(bookings);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Booking booking)
+        {
+            booking.Id = bookings.Count + 1;
+            bookings.Add(booking);
+            return RedirectToAction("Index");
+        }
+    }
+}
